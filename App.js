377e5db906256f57fe63da80e783dcc03c847a8a
cuongnbph22662 <bacuong15423@gmail.com>
2023-04-09 15:24:25 +0700

    int file

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React, { useState } from 'react';
+//import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function App() {
+import Login from './screens/login/Login';
+import TaoTaiKhoan from './screens/login/TaoTaiKhoan';
+import HomeScreen from './screens/tabScreen/HomeScreen';
+import SearchScreen from './screens/tabScreen/SearchScreen';
+import SettingScreen from './screens/tabScreen/SettingScreen';
+import DoiMatKhau from './screens/user/DoiMatKhau';
+import ThongTinCN from './screens/user/ThongTinCN';
+
+
+
+
+//cường nè
+const Tab = createBottomTabNavigator();
+const StackDemo = createNativeStackNavigator();
+
+function MyTabs() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Home" component={HomeScreen} options={{
+        tabBarIcon: () => <Image source={require('./assets/house.png')} style={{ width: 25, height: 25 }} resizeMode="stretch" />
+      }} />
+      <Tab.Screen name="Search" component={SearchScreen} options={{
+        tabBarIcon: () => <Image source={require('./assets/search.png')} style={{ width: 25, height: 25 }} resizeMode="stretch" />
+      }} />
+      <Tab.Screen name="Setting" component={SettingScreen} options={{
+        tabBarIcon: () => <Image source={require('./assets/settings.png')} style={{ width: 25, height: 25 }} resizeMode="stretch" />
+      }} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default function App() {
+  const [trangThai, settrangThai] = useState(false);
+
+  const kiemTraTrangThai = async () => {
+    try {
+      const value = await AsyncStorage.getItem(keys);
+      if (value !== null) {
+        settrangThai(true)
+      }
+
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  React.useEffect(() => {
+    kiemTraTrangThai()
+  })
+  return (
+    <NavigationContainer>
+      <StackDemo.Navigator>
+        {
+          !trangThai && <StackDemo.Screen name="Login" component={Login} options={({ headerShown: false })} />
+        }
+
+        <StackDemo.Screen name="Main" component={MyTabs} options={({ headerShown: false })} />
+        <StackDemo.Screen name="CreateAccount" component={TaoTaiKhoan} />
+        <StackDemo.Screen name="ThongTinCN" component={ThongTinCN} options={{ title: "Thông tin người dùng" }} />
+        <StackDemo.Screen name="DoiMatKhau" component={DoiMatKhau} options={{ title: "Đổi mật khẩu" }} />
+      </StackDemo.Navigator>
+    </NavigationContainer>
+  );
+}
+
